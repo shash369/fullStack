a17ed3a0deadf7b052cb797e096acdf8510f352e
shashwat shukla <shuklashashwat009@gmail.com>
2024-04-10 23:17:15 +0530

    text added

---
+++ async&sync.js
@@ -0,0 +1,45 @@
+// //sync->one at a time
+// //async->multitask
+
+// // let date =new Date();
+// // console.log(date.getSeconds());
+
+// // async function to(){
+// //     for (let index = 0; index < 1000000000; index++) { 
+// // }}
+// // to();
+// // let date2 =new Date();
+// // console.log(date2.getSeconds());
+
+
+// // const data=fs.readFileSync("a.txt","utf-8");
+// // console.log(data);
+
+// const { log } = require('console');
+// const fs=require('fs');
+
+// fs.readFile("a.txt","utf-8",(err,data)=>{
+//     if (err) {
+//         console.error("Error reading file:", err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+// log('hello');
+
+
+const fs=require('fs')
+
+function ReadFile1(){
+    return new Promise(function(resolve){
+        fs.readFile('a.txt','utf-8',(err,data)=>{
+            resolve(data);
+        })
+    })
+}
+
+function print(data){
+    console.log (data);
+}
+
+ReadFile1().then(print);
