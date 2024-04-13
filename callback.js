350bd4a393e436ab08e7f74573ae28192cf584f3
shashwat shukla <shuklashashwat009@gmail.com>
2024-04-13 23:24:25 +0530

    call back

---
+++ callback.js
@@ -0,0 +1,15 @@
+//revision
+function squae(n){
+//    console.log('square is called');
+    return n*n;
+}
+function cube(n){
+    return n*n*n; 
+} 
+function calculat(a,b,fun){
+    let alp1=fun(a);
+    let alp2=fun(b);
+
+    return alp1+alp2;
+}
+console.log(calculat(10,5,squae));
