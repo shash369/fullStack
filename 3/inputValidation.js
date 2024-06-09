ed2b241ab1d0688750ecd06834888a8a82948384
shashwat shukla <shuklashashwat009@gmail.com>
2024-06-09 15:45:23 +0530

    zod level 0

--- 3/inputValidation.js
+++ 3/inputValidation.js
@@ -1,1 +1,22 @@
-//use zod for schema validation 
+//using zod for schema validation 
+const express = require("express");
+const zod =require("zod")
+const app = express();
+const port = 4200;
+
+app.use(express.json());
+const schema=zod.array(zod.number())
+
+app.post("/arr", (req, res) => {
+   const kidney = req.body.kidney;
+   const respo=schema.safeParse(kidney)
+    if(respo.success){
+        res.send("number is: "+respo.data.length);
+    }
+    else{
+        res.send(respo.error);
+    }
+});
+app.listen(port, () => {
+    console.log("Connected at port: " + port);
+});
