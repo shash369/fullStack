f28ec23803b9a61c361220c969eac0ff6a456b11
shashwat shukla <shuklashashwat009@gmail.com>
2024-07-15 22:27:03 +0530

    ex

---
+++ promise/time.js
@@ -0,0 +1,10 @@
+async function sleep(time){
+    return new Promise((resolve)=>{
+        setTimeout(resolve, time);
+    })
+}
+
+let time=Date.now();
+sleep(100).then(()=>{
+    console.log(Date.now()-time)
+})
